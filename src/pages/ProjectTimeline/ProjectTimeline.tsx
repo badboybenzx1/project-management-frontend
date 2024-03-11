54ee3d7b0a85583c9fc6f7455c701557b7f55dc7
naphon.p <naphon.pha@gmail.com>
2024-03-11 17:15:22 +0700

    feat (grantt) ✨: add grantt chart

--- src/pages/ProjectTimeline/ProjectTimeline.tsx
+++ src/pages/ProjectTimeline/ProjectTimeline.tsx
@@ -1,6 +1,136 @@
 import React from "react";
+import {
+  Gantt,
+  Task,
+  // EventOption,
+  // StylingOption,
+  // ViewMode,
+  // DisplayOption,
+} from "gantt-task-react";
+import "gantt-task-react/dist/index.css";
+import Title from "antd/es/typography/Title";
+
+const currentDate = new Date();
+
 const ProjectTimeline = () => {
-  return <div>ProjectTimeline</div>;
+  const [tasks, setTasks] = React.useState<Task[]>([
+    {
+      start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 1),
+      end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 15),
+      name: "#Little Boy",
+      id: "ProjectSample",
+      progress: 25,
+      type: "project",
+      hideChildren: false,
+      displayOrder: 1,
+      styles: {
+        progressColor: "#1677ff",
+        progressSelectedColor: "#1677ff",
+        backgroundColor: "#aeb8c2",
+        backgroundSelectedColor: "red",
+      },
+    },
+    {
+      start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 1),
+      end: new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        2,
+        12,
+        28
+      ),
+      name: "Idea",
+      id: "Task 0",
+      progress: 45,
+      type: "task",
+      project: "ProjectSample",
+      displayOrder: 2,
+    },
+    {
+      start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 2),
+      end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 4, 0, 0),
+      name: "Research",
+      id: "Task 1",
+      progress: 25,
+      dependencies: ["Task 0"],
+      type: "task",
+      project: "ProjectSample",
+      displayOrder: 3,
+    },
+    {
+      start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 4),
+      end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 8, 0, 0),
+      name: "Discussion with team",
+      id: "Task 2",
+      progress: 10,
+      dependencies: ["Task 1"],
+      type: "task",
+      project: "ProjectSample",
+      displayOrder: 4,
+    },
+    {
+      start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 8),
+      end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 9, 0, 0),
+      name: "Developing",
+      id: "Task 3",
+      progress: 2,
+      dependencies: ["Task 2"],
+      type: "task",
+      project: "ProjectSample",
+      displayOrder: 5,
+    },
+    {
+      start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 8),
+      end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 10),
+      name: "Review",
+      id: "Task 4",
+      type: "task",
+      progress: 70,
+      dependencies: ["Task 2"],
+      project: "ProjectSample",
+      displayOrder: 6,
+    },
+    {
+      start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 15),
+      end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 15),
+      name: "Release",
+      id: "Task 6",
+      progress: currentDate.getMonth(),
+      type: "milestone",
+      dependencies: ["Task 4"],
+      project: "ProjectSample",
+      displayOrder: 7,
+    },
+    {
+      start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 18),
+      end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 19),
+      name: "Party Time",
+      id: "Task 9",
+      progress: 0,
+      isDisabled: true,
+      type: "task",
+    },
+  ]);
+
+  const handleExpanderClick = (task: Task) => {
+    setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
+    console.log("On expander click Id:" + task.id);
+  };
+
+  return (
+    <>
+      <Title level={3}>Project Timeline</Title>
+
+      <Gantt
+        tasks={tasks}
+        barProgressSelectedColor="#1677ff"
+        barProgressColor="#1677ff"
+        arrowColor="#75757545"
+        // locale="tha"
+        onExpanderClick={handleExpanderClick}
+      />
+    </>
+  );
 };
 
 export default ProjectTimeline;
